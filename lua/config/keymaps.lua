c295ac36c15efcab2043242331778176737c04bf
Nick <nickphilomath@proton.me>
2025-06-11 16:33:23 +0500

    remove scroll keymaps, change theme

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,15 +6,15 @@
 -- ~/.config/nvim/lua/config/keymaps.lua
 
 -- Set local variable for options (no "z" suffix)
-local opts = { noremap = true, silent = true }
+-- local opts = { noremap = true, silent = true }
 
 -- Map Ctrl+j to scroll forward (Ctrl+f behavior)
-vim.keymap.set("n", "<C-j>", "<C-f>", opts)
-vim.keymap.set("v", "<C-j>", "<C-f>", opts)
+-- vim.keymap.set("n", "<C-j>", "<C-f>", opts)
+-- vim.keymap.set("v", "<C-j>", "<C-f>", opts)
 
 -- Map Ctrl+k to scroll backward (Ctrl+b behavior)
-vim.keymap.set("n", "<C-k>", "<C-b>", opts)
-vim.keymap.set("v", "<C-k>", "<C-b>", opts)
+-- vim.keymap.set("n", "<C-k>", "<C-b>", opts)
+-- vim.keymap.set("v", "<C-k>", "<C-b>", opts)
 
 -- Optional: uncomment these if you want to disable the original Ctrl+f/b
 -- vim.keymap.set("n", "<C-f>", "<nop>", opts)
